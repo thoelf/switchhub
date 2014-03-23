3cff88e5763d6a763ad71c74c50d373fbbbd934a
Thomas <thomas.elfstrom@gmail.com>
2014-03-23 10:16:00 +0100

    Small changes to adopt some best practice Bash scripting.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #Copyright 2014 Thomas Elfström
 #install.sh
 
@@ -18,12 +18,13 @@
 # along with SwitchHub. If not, see <http://www.gnu.org/licenses/>.
 
 
-#resize -s 30 92 &> /dev/null
+
+resize -s 30 92 &> /dev/null
 
 echo -ne "\033]0;SwitchHub installation\007"
 clear
 
-if [ $(id -u) -ne 0 ]; then
+if [[ "$(id -u)" -ne 0 ]]; then
     printf "Run as root or with sudo.\n"
     exit 1
 fi
@@ -31,7 +32,7 @@ fi
 INSTALL_DIR=/opt/switchhub
 LOG_FILE=/var/log/switchhub.log
 SETTINGS_DIR=/etc/switchhub
-STARTDIR=$(pwd)
+STARTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "You are about to install SwitchHub. "
 printf "Press any key to continue the installation\nor Ctrl+C to quit."
@@ -39,53 +40,53 @@ read
 
 GEXIST=false
 getent group switchhub >/dev/null 2>&1 && GEXIST="true"
-if [ "$GEXIST" == "false" ]; then
+if [[ "$GEXIST" = "false" ]]; then
     groupadd switchhub
     printf "\nAdded the group switchhub.\n"
 fi
 
 cd ..
 
-mv -v $STARTDIR switchhub
+mv -v "$STARTDIR" switchhub
 
-if [ ! -d "/etc/switchhub" ]; then
-    mkdir $SETTINGS_DIR
-    printf "Created the directory $SETTINGS_DIR"
+if [[ ! -d "$SETTINGS_DIR" ]]; then
+    mkdir -p "$SETTINGS_DIR"
+    printf "Created the directory "$SETTINGS_DIR""
 fi
 
-mv -v ./switchhub/{events.cfg,free_days.cfg,holidays.cfg,program.cfg} $SETTINGS_DIR
-chgrp switchhub $SETTINGS_DIR/*
-chmod g+w $SETTINGS_DIR/*
+mv -v ./switchhub/{events.cfg,free_days.cfg,holidays.cfg,program.cfg} "$SETTINGS_DIR"
+chgrp switchhub "$SETTINGS_DIR"/*
+chmod g+w "$SETTINGS_DIR"/*
 
 mv -v ./switchhub/{switchhub_start,switchhub_status,switchhub_stop} .
 chmod u+x switchhub_start switchhub_status switchhub_stop
 
-if [ -d "$INSTALL_DIR" ]; then
-    rm -rf $INSTALL_DIR
-    printf "Removed old install directory $INSTALL_DIR\n"
+if [[ -d "$INSTALL_DIR" ]]; then
+    rm -rf "$INSTALL_DIR"
+    printf "Removed old install directory "$INSTALL_DIR"\n"
 fi
 
 mv -v switchhub /opt
-printf "Created new install directory $INSTALL_DIR\n"
-chown root:root $INSTALL_DIR
+printf "Created new install directory "$INSTALL_DIR"\n"
+chown root:root "$INSTALL_DIR"
 
-chgrp -R switchhub $INSTALL_DIR/*
-chmod -R g+w $INSTALL_DIR/*
-chmod g+x $INSTALL_DIR/switchhub.py
+chgrp -R switchhub "$INSTALL_DIR"/*
+chmod -R g+w "$INSTALL_DIR"/*
+chmod g+x "$INSTALL_DIR"/switchhub.py
 
 mv -v switchhub_logrotate /etc/logrotate.d/switchhub
 
-if [ ! -f "$LOG_FILE" ]; then
+if [[ ! -f "$LOG_FILE" ]]; then
     touch $LOG_FILE
-    printf "Created $LOG_FILE"
+    printf "Created "$LOG_FILE""
 fi
 
-chgrp switchhub $LOG_FILE
-chmod g+w $LOG_FILE
+chgrp switchhub "$LOG_FILE"
+chmod g+w "$LOG_FILE"
 
-printf "The installation directory is: $INSTALL_DIR\n"
+printf "The installation directory is: "$INSTALL_DIR"\n"
 printf "The configuration files are in the directory /etc/switchhub.\n"
-printf "The log file is: $LOG_FILE\n"
+printf "The log file is: "$LOG_FILE"\n"
 printf "The configuration file for log rotation is /etc/logrotate.d/switchhub\n"
 printf "The group switchhub was created.\n\n"
 printf "Press any key to quit."
